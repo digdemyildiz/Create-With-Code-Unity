fad68e896a8fa4ceefe95795e032fca098a15043
Digdem Yildiz <digdemyildiz19@ku.edu.tr>
2024-07-23 16:34:30 +0300

    added player control to rotate and move the vehicle

--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    private float speed = 10.0f;
+    private float turnSpeed = 25.0f;
+    private float horizontalInput;
+    private float verticalInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,10 +20,16 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Move the vehicle forward
-        // transform.Translate(0, 0, 1);
+        
+        // Get the inputs from the user
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
 
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        // Move the vehicle forward based on user input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+
+        // Rotate the vehicle based on user input
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
         
     }
 }
